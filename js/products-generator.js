2a3bf1830d1558bfc551136b52fa0bb2681e5f03
Carlos <carloscr95r@gmail.com>
2025-01-10 21:13:16 -0300

    update product generator
    Se corrige animación boton para display sm y md

--- js/products-generator.js
+++ js/products-generator.js
@@ -15,25 +15,39 @@ document.addEventListener("DOMContentLoaded", function () {
       products.forEach((product, index) => {
         
         const card = `
-        <div class="col product-item">
-          <div class="card h-100" style="border-radius: 0 !important;">
-            <div class="product-img border-bottom bg-white">
-              <div class="card-image-container position-relative overflow-hidden">
-                <img src="../assets/images/products/${product.main_image}" class="card-img-top main-image" alt="${product.title}">
-                <img src="../assets/images/products/${product.hover_image}" class="card-img-top hover-image position-absolute top-0 start-0 w-100 h-100" alt="${product.title}">
+            <div class="col product-item z-1">
+              <div class="product-card-container h-100">
+               <div class="h-100 position-relative p-0">
+                <div class="card h-100 border-sm-bottom-0" style="border-radius: 0 !important;">
+                  <div class="product-img border-bottom bg-white">
+                    <div class="card-image-container position-relative overflow-hidden">
+                      <img src="../assets/images/products/${product.main_image}" class="card-img-top main-image" alt="${product.title}">
+                      <img src="../assets/images/products/${product.hover_image}" class="card-img-top hover-image position-absolute top-0 start-0 w-100 h-100" alt="${product.title}">
+                    </div>
+                  </div>
+                  <div class="card-body bg-body-tertiary d-flex flex-column gap-1 py-2">
+                    <div class="product-vendor text-muted">${product.vendor}</div>
+                    <div class="product-title fw-bold">${product.title}</div>
+                    <ul class="list-unstyled mt-auto mb-0">
+                      <li><span class="text-dark-emphasis fw-bold">$${product.price.toLocaleString("es-AR")}</span></li>
+                      <li>${product.installments.quantity} cuotas de: <span class="text-dark-emphasis">$${product.installments.amount.toLocaleString("es-AR")}</span></li>
+                      <li><span class="text-dark-emphasis fw-bold">$${product.transfer_price.toLocaleString("es-AR")}</span> <small class="text-muted">con Transferencia</small></li>
+                    </ul>
+                  </div>
+                  </div>
+                  <div class="m-0 p-0">
+                    <div class="buy-button bg-body-tertiary top-0">
+                      <div class="p-4 pt-1">
+                        <button class="btn btn-warning w-100 z-5" type="button">
+                          Ver producto
+                        </button>
+                      </div>
+                    </div>
+                  </div>
+                  </div>
               </div>
             </div>
-            <div class="card-body bg-body-tertiary d-flex flex-column gap-1 pt-2 pb-3">
-              <div class="product-vendor text-muted">${product.vendor}</div>
-              <div class="product-title fw-bold">${product.title}</div>
-              <ul class="list-unstyled mt-auto mb-0">
-                <li><span class="text-dark-emphasis fw-bold">$${product.price.toLocaleString("es-AR")}</span></li>
-                <li>${product.installments.quantity} cuotas de: <span class="text-dark-emphasis">$${product.installments.amount.toLocaleString("es-AR")}</span></li>
-                <li><span class="text-dark-emphasis fw-bold">$${product.transfer_price.toLocaleString("es-AR")}</span> <small class="text-muted">con Transferencia</small></li>
-              </ul>
-            </div>
-          </div>
-        </div>
+
         `;
         
         container.innerHTML += card;
